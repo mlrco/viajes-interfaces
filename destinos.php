23a14fbdf5f4df1c8ff9e64bba56f96275712596
marco <48846555+mlrco@users.noreply.github.com>
2024-11-05 10:07:54 -0700

    Update destinos.php

--- destinos.php
+++ destinos.php
@@ -34,9 +34,9 @@
 
     <nav class="navbar">
       <ul>
-        <li><a href="index.php">PAGINA PRINCIPAL</a></li>
-        <li><a href="contacto.php">CONTACTO</a></li>
-        <li><a href="acercaDe copy.php">SOBRE NOSOTROS</a></li>
+        <li><a href="index.php">Página principal</a></li>
+        <li><a href="contacto.php">Contacto</a></li>
+        <li><a href="acercaDe copy.php">Sobre nosotros</a></li>
 
       </ul>
     </nav>
@@ -44,7 +44,7 @@
 
     <nav class="navLogin">
       <ul>
-        <li><a href="login/login.php" class="login">CERRAR SESIÓN</a></li>
+        <li><a href="login/login.php" class="login">Cerrar Sesión</a></li>
 
       </ul>
     </nav>
@@ -59,7 +59,7 @@
     <div class="container1">
       <div></div>
       <div>
-        <h1>DESTINOS</h1>
+        <h1>Destinos</h1>
         <hr>
       </div>
     </div>
@@ -81,7 +81,7 @@
       <p>Es conocida por sus playas con fuerte oleaje, como la playa Sayulita central. Al oeste, la Playa de los Muertos está más resguardada entre rocas que la protegen. Las galerías de Sayulita venden obras de arte del pueblo indígena huichol. Al suroeste, las Islas Marietas tienen una fauna diversa que incluye ballenas jorobadas y delfines.</p>
       <p>Tiempo: 31°C, viento del SO a 11 km/h, humedad del 73 %.</p>
       <div class="btn">
-        <a href="destinos/Sayulita/sayulita.php">LEER MÁS</a>
+        <a href="destinos/Sayulita/sayulita.php">Leer más</a>
       </div>
       
     </div>
@@ -101,7 +101,7 @@
       <p>San Blas es una ciudad y puerto del estado mexicano de Nayarit, ubicada de cara al océano Pacífico. Es la cabecera del municipio de San Blas. Fue fundado en época de la colonia española y fue uno de los dos principales puertos en el océano Pacífico del Virreinato de Nueva España, junto con Acapulco.</p>
       <p>Superficie 823,6 km², Altitud 10m s.n.m. Población (2010) 10 187 hab.​</p>
       <div class="btn">
-        <a href="destinos/San Blas/San Blas.php">LEER MÁS</a>
+        <a href="destinos/San Blas/San Blas.php">Leer más</a>
       </div>
     </div>
   </section>
@@ -113,7 +113,7 @@
       <p>El Palacio de Gobierno fue inicialmente construido en el período 1870-1885, para que fuera una cárcel de la que aún se mantiene la distribución de espacios de los presidios de la época.</p>
       <p>Es un edificio de 2 plantas con altos ventanales y balcones. El centro de la fachada está rematado por un amplio frontón con reloj.</p>
       <div class="btn">
-        <a href="destinos/Tepic/tepic.php">LEER MÁS</a>
+        <a href="destinos/Tepic/tepic.php">Leer más</a>
       </div>
       
     </div>
@@ -132,7 +132,7 @@
       <p>Con su playa de suave oleaje y arena blanca, Rincón de Guayabitos es uno de los mejores lugares para visitar en Nayarit por también su amplia variedad de alojamientos. Encontrarás desde cabañas rusticas a pie de arenal, hasta hoteles elegantes y modernos.</p>
       <p>En la playa se celebra ocasionalmente la ceremonia de liberación de tortuguillos que parten a hacer su vida. Desde ella también salen excursiones de pesca y de avistamiento de ballenas y fauna marina.</p>
       <div class="btn">
-        <a href="destinos/RinconGuayabitos/guayabitos.php">LEER MÁS</a>
+        <a href="destinos/RinconGuayabitos/guayabitos.php">Leer más</a>
       </div>
     </div>
   </section>
@@ -144,7 +144,7 @@
       <p>Esta isla del municipio de Santiago Ixcuintla es uno de los rincones más raros y encantadores de Nayarit, con sus casas de techos entejados de dos aguas y calles inundadas por el desbordamiento del río San Pedro</p>
       <p>Los artesanos son hábiles trabajando el mangle y las hojas de palma con las que fabrican barcinas, esferas donde guardan camarones secos.</p>
       <div class="btn">
-        <a href="destinos/Isla de Mexcaltitan/mexcaltitan.php">LEER MÁS</a>
+        <a href="destinos/Isla de Mexcaltitan/mexcaltitan.php">Leer más</a>
       </div>
     </div>
     <div class="left">
@@ -163,7 +163,7 @@
       <p>Es una península de poco más de 6 km2 con hoteles y condominios lujosos, caracterizada por un clima estable y fresca brisa marina.</p>
       <p> Allí también encontrarás el Cerro Careyeros con vestigios de vida del siglo IX.</p>
       <div class="btn">
-        <a href="destinos/Punta de Mita/PuntaDeMita.php">LEER MÁS</a>
+        <a href="destinos/Punta de Mita/PuntaDeMita.php">Leer más</a>
       </div>
     </div>
   </section>
